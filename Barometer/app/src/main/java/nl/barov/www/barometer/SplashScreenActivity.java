fdfbe62cc4e0de587e7a315568642b1a29a4634e
Tundzaj Barov <t.barov@gmail.com>
2016-05-03 13:03:55 +0200

    Splash screen klaar geen actionbar meer

--- Barometer/app/src/main/java/nl/barov/www/barometer/SplashScreenActivity.java
+++ Barometer/app/src/main/java/nl/barov/www/barometer/SplashScreenActivity.java
@@ -6,24 +6,28 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // ANIMATIE LOGO HSLEIDEN
+        ImageView logo_hsleiden = (ImageView) findViewById(R.id.logo_hsleiden);
+        Animation myFadeInAnimation_1500 = AnimationUtils.loadAnimation(this, R.anim.fadein_1500);
+        logo_hsleiden.startAnimation(myFadeInAnimation_1500);
+
     }
-
 }
